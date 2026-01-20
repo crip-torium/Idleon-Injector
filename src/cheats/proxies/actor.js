5546d919d8bd1577c2b8403551a280ed61920c10
Arnold W <arnoldwildt@gmail.com>
2026-01-20 18:45:37 +0100

    remove guard from actor

--- src/cheats/proxies/actor.js
+++ src/cheats/proxies/actor.js
@@ -9,15 +9,10 @@ import { ActorModel } from "../core/globals.js";
  * Setup Actor model proxies.
  */
 export function setupActorProxies() {
-    if (!ActorModel || !ActorModel.prototype) return;
-    if (ActorModel.prototype.internalUpdate._isPatched) return;
-
     const originalInternalUpdate = ActorModel.prototype.internalUpdate;
 
     ActorModel.prototype.internalUpdate = function (elapsed, isAnimationTime) {
         const animTime = cheatState.wide?.noanim ? false : isAnimationTime;
         return Reflect.apply(originalInternalUpdate, this, [elapsed, animTime]);
     };
-
-    ActorModel.prototype.internalUpdate._isPatched = true;
 }
